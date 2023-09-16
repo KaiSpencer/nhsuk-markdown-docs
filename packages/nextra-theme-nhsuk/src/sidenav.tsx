57decabda885f29617eecbbcb7d0829162901743
Kai <kaispencer98@gmail.com>
2023-09-16 17:04:35 +0100

    Tests on PR

--- packages/nextra-theme-nhsuk/src/sidenav.tsx
+++ packages/nextra-theme-nhsuk/src/sidenav.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
-import type { Folder, PageMapItem, PageOpts } from "nextra";
+import type { Folder, Meta, PageMapItem, PageOpts } from "nextra";
 
-export function pageTitlesFromPageMapItems(pageMapItems: PageMapItem[]) {
+export function pageTitlesFromPageMapItems(pageMapItems: PageMapItem[]): Meta {
   for (const pageMapItem of pageMapItems) {
     if (pageMapItem.kind === "Meta") {
       return pageMapItem.data;
     }
   }
+  return {};
 }
 
 function RenderFolder({
