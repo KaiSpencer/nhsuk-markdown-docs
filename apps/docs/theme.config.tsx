4dbf10de6687e0d74412041dd4d6464d6b58bc98
Kai <kaispencer98@gmail.com>
2023-09-30 17:32:42 +0100

    Remove 'Docs' link from header in theme config .

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -5,6 +5,5 @@ export default {
     link: "https://github.com/shuding/nextra",
   },
   logo: <strong>Project</strong>,
-  headerLinks: [{ href: "/docs", text: "Docs" }],
   headerName: "NHSUK Markdown Docs",
 } satisfies ThemeConfig;
